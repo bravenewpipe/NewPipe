30d9e598ef7c8b4418abadcc8ac4e69c968e4344
evermind <evermind@tuxfamily.org>
2024-07-25 23:10:08 +0200

    BraveNewPipe Release 0.27.2-2.2.0
    - sync with NewPipe 0.27.2

--- app/src/main/java/org/schabi/newpipe/BraveNewVersionWorkerHelper.kt
+++ app/src/main/java/org/schabi/newpipe/BraveNewVersionWorkerHelper.kt
@@ -8,9 +8,8 @@ object BraveNewVersionWorkerHelper {
 
     fun getVersionInfo(response: Response): JsonObject {
         val newpipeVersionInfo = JsonParser.`object`()
-                .from(response.responseBody()).getObject("flavors")
-                .getObject("github").getObject("stable")
+            .from(response.responseBody()).getObject("flavors")
+            .getObject("github").getObject("stable")
         return newpipeVersionInfo
     }
-
 }
